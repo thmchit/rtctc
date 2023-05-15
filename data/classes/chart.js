8fdb7caec81ccadafb9f96df4c31326292960aa1
Junee <juneekim7@gmail.com>
2023-05-16 00:32:49 +0900

    use rhy-game

--- data/classes/chart.js
+++
@@ -1,6 +0,0 @@
-export class Chart {
-    constructor({ difficulty = 0, lanes = [], transformer = [] } = {}) {
-        [ this.difficulty, this.lanes, this.transformer ]
-        = [ difficulty, lanes, transformer ]
-    }
-}
