59f1c14492a917df6cf8d5465dbddfc8da0dc51d
Junee <juneekim7@gmail.com>
2023-05-20 06:32:36 +0900

    setting delay

--- client/settings.js
+++ client/settings.js
@@ -4,5 +4,6 @@ const settings = {
         // down: ['d', 'f', 'g', 'h', 'j', 'k'],
         down: ['s', 'd', 'f', 'j', 'k', 'l'],
         up: ['w', 'e', 'r', 'u', 'i', 'o']
-    }
+    },
+    delay: -400
 }
