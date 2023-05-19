fae5266ef4570857a5c86e45c995d9b3041ba20e
Junee <juneekim7@gmail.com>
2023-05-19 23:48:55 +0900

    setting 주석

--- client/settings.js
+++ client/settings.js
@@ -1,6 +1,7 @@
 const settings = {
     keybind: {
         //Lower
+        // down: ['d', 'f', 'g', 'h', 'j', 'k'],
         down: ['s', 'd', 'f', 'j', 'k', 'l'],
         up: ['w', 'e', 'r', 'u', 'i', 'o']
     }
