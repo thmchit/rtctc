a07fd99d6251e163407475dcd48429990b10286a
Junee <juneekim7@gmail.com>
2023-05-20 06:42:20 +0900

    settings

--- client/settings.js
+++ client/settings.js
@@ -1,7 +1,6 @@
 const settings = {
     keybind: {
         //Lower
-        // down: ['d', 'f', 'g', 'h', 'j', 'k'],
         down: ['s', 'd', 'f', 'j', 'k', 'l'],
         up: ['w', 'e', 'r', 'u', 'i', 'o']
     },
