9e1a739b698e2bbc84a6e60e3a1e125dc3716b36
Raehwan0418 <raehwan0418@gmail.com>
2023-05-23 19:56:01 +0900

    i am

--- data/mapList.js
+++ data/mapList.js
@@ -1,5 +1,5 @@
 const mapList = []
-const mapNames = ['LOVE DIVE', '다신 보지 않을거야.', '염라']
+const mapNames = ['LOVE DIVE', '다신 보지 않을거야.', '염라', 'I AM']
 
 for (const mapName of mapNames) {
     document.write(`<script src="../../data/song/${mapName}.js"></script>\n`)
